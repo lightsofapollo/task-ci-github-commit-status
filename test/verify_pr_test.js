40f8160ae886b85cdf99ad40eceb07516a3571e9
James Lal <james@lightsofapollo.com>
2013-09-26 21:34:15 -0700

    closed + no status

--- test/verify_pr_test.js
+++ test/verify_pr_test.js
@@ -15,6 +15,7 @@ suite('verify_pr', function() {
   function pullRequest(overrides) {
     var base = {
       mergeable: true,
+      state: 'open',
       base: {
         sha: 'myshaisbetterthenyoursha'
       }
@@ -108,6 +109,81 @@ suite('verify_pr', function() {
           success: false,
           state: STATES.CANNOT_MERGE.state,
           message: STATES.CANNOT_MERGE.message
+        });
+
+        done();
+      }
+    );
+  });
+
+  test('pull request is closed', function(done) {
+    // input of operation
+    var input = { user: user, repo: repo, number: prNumber };
+
+    // stub predefined output
+    var pr = pullRequest({ state: 'closed' });
+
+    // pull request get success
+    var get = this.sinon.stub(github.pullRequests, 'get');
+    get.callsArgWithAsync(1, null, pr);
+
+    verify(
+      { user: user, repo: repo, number: prNumber },
+      function(err, result) {
+        // calls get
+        assert.calledWithMatch(get, sinon.match(input), sinon.match.any);
+
+        // result of successful operation
+        assert.ok(!err, 'there is no err');
+        assert.deepEqual(result, {
+          success: false,
+          state: STATES.CLOSED.state,
+          message: STATES.CLOSED.message
+        });
+
+        done();
+      }
+    );
+  });
+
+  test('no status', function(done) {
+    // input of operation
+    var input = { user: user, repo: repo, number: prNumber };
+
+    // stub predefined output
+    var pr = pullRequest(),
+        statusList = pullRequestStatus([]);
+
+    // pull request get success
+    var get = this.sinon.stub(github.pullRequests, 'get');
+    get.callsArgWithAsync(1, null, pr);
+
+    // status request success
+    var status = this.sinon.stub(github.statuses, 'get');
+    status.callsArgWithAsync(1, null, statusList);
+
+    verify(
+      { user: user, repo: repo, number: prNumber },
+      function(err, result) {
+        // calls get
+        assert.calledWithMatch(get, sinon.match(input), sinon.match.any);
+
+        // get pr is before status check
+        assert(get.calledBefore(status), 'gets before status');
+
+        // status check
+        assert.calledWithMatch(
+          status,
+          sinon.match({ user: user, repo: repo, sha: pr.base.sha }),
+          sinon.match.any
+        );
+
+        // result of successful operation
+        assert.ok(!err, 'there is no err');
+        assert.deepEqual(result, {
+          success: false,
+          state: STATES.CI_NO_STATUS.state,
+          message: STATES.CI_NO_STATUS.message
         });
 
         done();
