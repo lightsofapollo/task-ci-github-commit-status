5c3d9ef1b4455a507143387527a5f870bbfcd734
James Lal <james@lightsofapollo.com>
2013-09-26 21:18:38 -0700

    handle cases where we cannot merge the PR

--- test/verify_pr_test.js
+++ test/verify_pr_test.js
@@ -2,6 +2,8 @@ suite('verify_pr', function() {
   var verify = require('../lib/verify_pr'),
       github = require('../lib/github');
 
+  var STATES = require('../lib/error_states');
+
   setup(function() {
     this.sinon = sinon.sandbox.create();
   });
@@ -36,53 +38,81 @@ suite('verify_pr', function() {
     return result;
   }
 
-  suite('verify pull request', function() {
-    var user = 'james',
-        repo = 'gaia',
-        prNumber = 3333;
+  var user = 'james',
+      repo = 'gaia',
+      prNumber = 3333;
 
-    test('success', function(done) {
-      // input of operation
-      var input = { user: user, repo: repo, number: prNumber };
+  test('success', function(done) {
+    // input of operation
+    var input = { user: user, repo: repo, number: prNumber };
 
-      // stub predefined output
-      var pr = pullRequest(),
-          statusList = pullRequestStatus(['fail', 'success']);
+    // stub predefined output
+    var pr = pullRequest(),
+        statusList = pullRequestStatus(['fail', 'success']);
 
-      // pull request get success
-      var get = this.sinon.stub(github.pullRequests, 'get');
-      get.callsArgWithAsync(1, null, pr);
+    // pull request get success
+    var get = this.sinon.stub(github.pullRequests, 'get');
+    get.callsArgWithAsync(1, null, pr);
 
-      // status request success
-      var status = this.sinon.stub(github.statuses, 'get');
-      status.callsArgWithAsync(1, null, statusList);
+    // status request success
+    var status = this.sinon.stub(github.statuses, 'get');
+    status.callsArgWithAsync(1, null, statusList);
 
-      verify(
-        { user: user, repo: repo, number: prNumber },
-        function(err, result) {
-          // calls get
-          assert.calledWithMatch(get, sinon.match(input), sinon.match.any);
+    verify(
+      { user: user, repo: repo, number: prNumber },
+      function(err, result) {
+        // calls get
+        assert.calledWithMatch(get, sinon.match(input), sinon.match.any);
 
-          // get pr is before status check
-          assert(get.calledBefore(status), 'gets before status');
+        // get pr is before status check
+        assert(get.calledBefore(status), 'gets before status');
 
-          // status check
-          assert.calledWithMatch(
-            status,
-            sinon.match({ user: user, repo: repo, sha: pr.base.sha }),
-            sinon.match.any
-          );
+        // status check
+        assert.calledWithMatch(
+          status,
+          sinon.match({ user: user, repo: repo, sha: pr.base.sha }),
+          sinon.match.any
+        );
 
-          // result of successful operation
-          assert.ok(!err, 'there is no err');
-          assert.deepEqual(result, {
-            success: true
-          });
+        // result of successful operation
+        assert.ok(!err, 'there is no err');
+        assert.deepEqual(result, {
+          success: true
+        });
 
-          done();
-        }
-      );
-    });
-
+        done();
+      }
+    );
   });
+
+  test('not mergeable', function(done) {
+    // input of operation
+    var input = { user: user, repo: repo, number: prNumber };
+
+    // stub predefined output
+    var pr = pullRequest({ mergeable: false });
+
+    // pull request get success
+    var get = this.sinon.stub(github.pullRequests, 'get');
+    get.callsArgWithAsync(1, null, pr);
+
+    verify(
+      { user: user, repo: repo, number: prNumber },
+      function(err, result) {
+        // calls get
+        assert.calledWithMatch(get, sinon.match(input), sinon.match.any);
+
+        // result of successful operation
+        assert.ok(!err, 'there is no err');
+        assert.deepEqual(result, {
+          success: false,
+          state: STATES.CANNOT_MERGE.state,
+          message: STATES.CANNOT_MERGE.message
+        });
+
+        done();
+      }
+    );
+  });
+
 });
